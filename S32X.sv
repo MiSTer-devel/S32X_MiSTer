89b9f3e85ec222043a9303c10628bef54b631b0d
Takiiiiiiii <43725718+Takiiiiiiii@users.noreply.github.com>
2025-12-05 21:58:30 +0800

    Update framework

--- S32X.sv
+++ S32X.sv
@@ -58,6 +58,8 @@ module emu
 	input  [11:0] HDMI_WIDTH,
 	input  [11:0] HDMI_HEIGHT,
 	output        HDMI_FREEZE,
+	output        HDMI_BLACKOUT,
+	output        HDMI_BOB_DEINT,
 
 `ifdef MISTER_FB
 	// Use framebuffer in DDRAM
@@ -184,8 +186,11 @@ assign LED_DISK  = 0;
 assign LED_POWER = 0;
 assign LED_USER  = rom_download | sav_pending;
 
-assign VGA_SCALER= 0;
+assign VGA_SCALER  = 0;
 assign VGA_DISABLE = 0;
+assign HDMI_FREEZE = 0;
+assign HDMI_BLACKOUT = 0;
+assign HDMI_BOB_DEINT = 0;
 
 assign AUDIO_S = 1;
 assign AUDIO_MIX = 0;
